5ed52eb261f0dbc047751281406cc22f937c83a5
Ruslan <suhorukovrus0@gmail.com>
2024-10-01 13:29:11 +0500

    complete

--- HW3.java
+++ HW3.java
@@ -1,26 +1,38 @@
 package ru.kaznu.ruslan.lesson8;
 
-import java.util.Arrays;
-
 public class HW3 {
+
+    final static int SIZE = 5;
+
     public static void main(String[] args) {
-        //Реализовать метод sumOfPositiveElements(..),
-        // принимающий в качестве аргумента целочисленный двумерный массив,
-        // метод должен посчитать и вернуть сумму всех элементов массива, которые больше 0;
-        int[][] array = {{1, -2}, {1, 2}, {1, 2}, {1, 2}};
-        arraySumElements(array);
+        //Реализовать метод, принимающий в качестве аргумента
+        // двумерный целочисленный массив, и зануляющий его диагональные
+        // элементы (можете выбрать любую из диагоналей, или занулить обе);
+        char[][] array = new char[SIZE][SIZE];
+
+        setZeroDiagonal(array);
     }
 
-    public static void arraySumElements(int[][] array) {
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                int value = array[i][j];
-                if (value > 0) {
-                    sum += value;
-                }
+    public static void setZeroDiagonal(char[][] array) {
+        // Заполнение массива * и его отрисовка
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                array[i][j] = '*';
+                System.out.print(array[i][j]);
             }
+            System.out.println();
         }
-        System.out.println("Сумма элементов массива: " + sum);
+        // Цикл зануления диагоналей
+        for (int i = 0; i < SIZE; i++) {
+
+            array[i][i] = 0; //Зануление первой диагонали
+            array[i][SIZE - 1 - i] = 0; // зануление второй диагонали
+
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
+
