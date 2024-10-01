f2f9f77279b42cee93a54bd1d9807b41ba193e40
Ruslan <suhorukovrus0@gmail.com>
2024-10-01 13:11:00 +0500

    complete

--- HW3.java
+++ HW3.java
@@ -1,26 +1,21 @@
 package ru.kaznu.ruslan.lesson8;
 
-import java.util.Arrays;
-
 public class HW3 {
+    static final int SIZE = 8;
+
     public static void main(String[] args) {
-        //Реализовать метод sumOfPositiveElements(..),
-        // принимающий в качестве аргумента целочисленный двумерный массив,
-        // метод должен посчитать и вернуть сумму всех элементов массива, которые больше 0;
-        int[][] array = {{1, -2}, {1, 2}, {1, 2}, {1, 2}};
-        arraySumElements(array);
+
+        printSquare(SIZE);
     }
 
-    public static void arraySumElements(int[][] array) {
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                int value = array[i][j];
-                if (value > 0) {
-                    sum += value;
-                }
+    public static void printSquare(int size) {
+        // Цикл  печати квадрата из *
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("* ");
             }
+            System.out.println(); // переход на новую строку после печати предыдущей
         }
-        System.out.println("Сумма элементов массива: " + sum);
     }
 }
+
