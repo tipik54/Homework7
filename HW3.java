9ef754954bf240bad7a259bc828b398a30f5fd23
Ruslan <suhorukovrus0@gmail.com>
2024-10-01 13:47:10 +0500

    start

--- HW3.java
+++ HW3.java
@@ -2,28 +2,16 @@ package ru.kaznu.ruslan.lesson8;
 
 public class HW3 {
     public static void main(String[] args) {
-        // Реализовать метод findMax(int[][] array) который должен найти и вернуть максимальный элемент массива;
-        int[][] array = {{1, 2, 3}, {9, 5, 6}, {7, 8, 4}};
+        // Реализуйте метод, который считает сумму
+        // элементов второй строки двумерного массива,
+        // если второй строки не существует, то в качестве результата необходимо вернуть -1
+        int[][] array = {{0,2}, {3,4}, {5,6}};
 
-        int maxElement = findMax(array);
-        System.out.println("Наибольший элемент массива: " + maxElement);
+        sumSecondRow(array);
     }
 
-    public static int findMax(int[][] array) {
-        // Задаем начальное значение максимума для сравнения элементов массива с ним
-        int max = array[0][0];
-
-        // При помощи цикла сравниваем каждый элемент массива и обновляем данные переменной int max
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (array[i][j] > max) {
-                    max = array[i][j];
-                }
-            }
-        }
-
-        return max; //возвращаем переменную max
+    public static void sumSecondRow(int[][] array) {
+        for (int i = 0; )
     }
 }
-
 
